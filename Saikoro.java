493be0b3be9d952a6fd951474a412dd6a623195f
Hiro-mn <54801478+Hiro-mn@users.noreply.github.com>
2020-03-31 01:13:13 +0900

    サイコロを作ってみました

---
+++ Saikoro.java
@@ -0,0 +1,7 @@
+public class Saikoro {
+    public static void main(String[] args) {
+        double rand = Math.random() * 7;
+        int number = (int) rand;
+        System.out.println("サイコロの目は" + number + "です");
+    }
+}
